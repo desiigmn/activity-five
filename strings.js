b3bbd2abf4376793c3769adfa9ae57d276655fbc
Desiree Ann Guzman <desireeann000@gmail.com>
2024-09-13 10:41:21 +0800

    Initial Commit

---
+++ strings.js
@@ -0,0 +1,28 @@
+// My favorite actor's first name.
+let favActorFirstName = "Enrique";
+
+//my favorite actor's last name.
+let favActorLastName = "Gil";
+
+// My favorite actor's fullname.
+let fullName = favActorFirstName + " " + favActorLastName;
+
+// Lowercase 
+let uppercase = fullName.toLowerCase();
+
+// Uppercase
+let lowercase = "Maui".toUpperCase(); 
+
+// Message
+let message = "My favorite actor is " + fullName.toUpperCase();
+
+// Append
+message += ", his best show is Silicon Valley.";
+
+// Log all variables to the console
+console.log(favActorFirstName);
+console.log(favActorLastName);
+console.log(fullName);
+console.log(uppercase);
+console.log(lowercase);
+console.log(message);
